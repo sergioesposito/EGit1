27bc17faaf5b22d9af6ac1f8703b3ac1115b0d88
bk0802 <bk0802@192.168.7.8>
2013-10-19 12:23:46 +0200

    CD1

---
+++ src/egit/Clase2.java
@@ -0,0 +1,19 @@
+package egit;
+
+public class Clase2 extends Clase1 {
+	private float sueldo;
+
+	public Clase2(int id, String nombre, float sueldo) {
+		super(id, nombre);
+		this.sueldo = sueldo;
+	}
+
+	public float getSueldo() {
+		return sueldo;
+	}
+
+	public void setSueldo(float sueldo) {
+		this.sueldo = sueldo;
+	}
+
+}
