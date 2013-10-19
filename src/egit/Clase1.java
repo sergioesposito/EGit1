8e8976e5a46a4040511b3be8cd4cdb60dc0bc750
bk0802 <bk0802@192.168.7.8>
2013-10-19 11:47:41 +0200

    Inicial sergio.antonio.esposito.perez

---
+++ src/egit/Clase1.java
@@ -0,0 +1,29 @@
+package egit;
+
+public class Clase1 {
+	private int id;
+	private String nombre;
+
+	public Clase1(int id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
